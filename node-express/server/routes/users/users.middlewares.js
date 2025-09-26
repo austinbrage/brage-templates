f0b98f8d55d77f4d3e5580aa363d70a4a64583b0
Austin Brage <agustinbrage19@gmail.com>
2025-09-26 19:45:04 -0300

    feat: move user validations from controller to middlewares

--- node-express/server/routes/users/users.middlewares.js
+++ node-express/server/routes/users/users.middlewares.js
@@ -1,32 +1,67 @@
 import { createErrorResponse } from './../../global/utils/responses.js';
-import { UsersStringValidation } from './users.validations.js';
+import { UsersStringValidation, UsersValidation } from './users.validations.js';
 
-class SanitizeGetRequests {
-
+class SanitizeRequests {
     constructor() {
+        this.validateUsers = new UsersValidation();
         this.validateStringUsers = new UsersStringValidation();
     }
 
     validationErr(res, validationError) {
-        return res.status(400).json(createErrorResponse({
-            message: 'Validation data error',
-            error: validationError.format()
-        }));
+        return res.status(400).json(
+            createErrorResponse({
+                message: 'Validation data error',
+                error: validationError.format(),
+            })
+        );
     }
-    
+
     getByID = (req, res, next) => {
-        const validation = this.validateStringUsers.getByID(req.query);
+        // 1. Validate input as string
+        let validation = this.validateStringUsers.getByID(req.query);
         if (!validation.success) return this.validationErr(res, validation.error);
 
-        // Initialize req.sanitizedData
-        req.sanitizedData = {};
-        
-        //id goes from string to number
-        req.sanitizedData.id = parseInt(validation.data.id, 10);
-        
+        // 2. Convert to the correct types
+        const convertedData = {};
+
+        // ➡️ id goes from string to number
+        convertedData.id = parseInt(validation.data.id, 10);
+
+        // 3. Validate the correct types
+        validation = this.validateUsers.getByID(convertedData);
+        if (!validation.success) return this.validationErr(res, validation.error);
+
+        req.sanitizedData = validation.data;
+
         next();
-    }
-    
+    };
+
+    addNew = (req, res, next) => {
+        const validation = this.validateUsers.addNew(req.body);
+        if (!validation.success) return this.validationErr(res, validation.error);
+
+        req.sanitizedData = validation.data;
+
+        next();
+    };
+
+    changeAll = (req, res, next) => {
+        const validation = this.validateUsers.changeAll(req.body);
+        if (!validation.success) return this.validationErr(res, validation.error);
+
+        req.sanitizedData = validation.data;
+
+        next();
+    };
+
+    remove = (req, res, next) => {
+        const validation = this.validateUsers.remove(req.body);
+        if (!validation.success) return this.validationErr(res, validation.error);
+
+        req.sanitizedData = validation.data;
+
+        next();
+    };
 }
 
-export default SanitizeGetRequests;
+export default SanitizeRequests;
--- node-express/server/routes/users/users.router.js
+++ node-express/server/routes/users/users.router.js
@@ -1,20 +1,20 @@
 import { Router } from 'express';
-import SanitizeGetRequests from './users.middlewares.js';
+import SanitizeRequests from './users.middlewares.js';
 import UsersController from './users.controllers.js';
 
 const createUsersRouter = ({ usersModel }) => {
     const usersRouter = Router();
 
-    const sanitize = new SanitizeGetRequests();
+    const sanitize = new SanitizeRequests();
     const usersController = new UsersController({ usersModel });
 
     usersRouter.get('/getAll', usersController.getAll);
     usersRouter.get('/getByID', sanitize.getByID, usersController.getByID);
-    usersRouter.post('/addNew', usersController.addNew);
-    usersRouter.put('/changeAll', usersController.changeAll);
-    usersRouter.delete('/remove', usersController.remove);
-    
+    usersRouter.post('/addNew', sanitize.addNew, usersController.addNew);
+    usersRouter.put('/changeAll', sanitize.changeAll, usersController.changeAll);
+    usersRouter.delete('/remove', sanitize.remove, usersController.remove);
+
     return usersRouter;
-}
+};
 
 export default createUsersRouter;
